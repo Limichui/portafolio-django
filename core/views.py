6d18db6a39e0767d15e150f255850c00fef2bf11
Limichui <limbert.olmos.m@gmail.com>
2024-04-18 22:51:49 -0400

    Adaptación de las planillas de navbar con herencia

--- core/views.py
+++ core/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-    <h1>Mi Web Personal</h1>
-    <ul>
-        <li><a href="/">Inicio</a></li>
-        <li><a href="/acerca-de/">Acerca de</a></li>
-        <li><a href="/portafolio/">Portafolio</a></li>
-        <li><a href="/contacto/">Contacto</a></li>
-    </ul>
-"""
-
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
 
 def about(request):
-    return render(request, "core/home.html")
+    return render(request, "core/about.html")
 
 def portfolio(request):
     return render(request, "core/portfolio.html")
